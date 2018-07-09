a225d9526ed768b9ffab45614649ee2b98f785dc
zhangyouzhi <zhangyouzhi@juxinli.com>
2018-07-09 15:30:09 +0800

    初次成形

--- src/main/java/com/wenkubao/WenkubaoApplication.java
+++ src/main/java/com/wenkubao/WenkubaoApplication.java
@@ -1,4 +1,4 @@
-package com.wenkubao.wenkubao;
+package com.wenkubao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/wenkubao/WenkubaoApplicationTests.java
+++ src/test/java/com/wenkubao/WenkubaoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wenkubao.wenkubao;
+package com.wenkubao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
