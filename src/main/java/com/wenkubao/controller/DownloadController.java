5289722d7f1c60bdce87f8bab99142b4df300288
zhangyouzhi <zhangyouzhi@juxinli.com>
2018-07-09 18:04:21 +0800

    版本1.0

--- src/main/java/com/wenkubao/controller/DownloadController.java
+++ src/main/java/com/wenkubao/controller/DownloadController.java
@@ -1,8 +1,14 @@
 package com.wenkubao.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class DownloadController {
+
+    @RequestMapping("/index")
+    public String index(){
+        return "main";
+    }
 
 }
